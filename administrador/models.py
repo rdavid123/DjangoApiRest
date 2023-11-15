6e014fdc9da8a9e1af1b66af4f87dfb5c89cf26f
David <david.ruiz@tecsup.edu.pe>
2023-11-15 04:05:13 -0500

    commit

--- administrador/models.py
+++ administrador/models.py
@@ -17,6 +17,7 @@ class User(models.Model):
     correo = models.EmailField(max_length=50)
     telefono = models.CharField(max_length=9)
     dni = models.CharField(max_length=8)
+    avatar = models.ImageField(upload_to='imagenes/',default='imagenes/default.jpg')
     password = models.CharField(max_length=100)
     rol = models.ForeignKey(Role, on_delete=models.CASCADE)
     def __str__(self):
@@ -33,17 +34,22 @@ class Servicio(models.Model):
 
 class Pedido(models.Model):
     cliente = models.ForeignKey(User, on_delete=models.CASCADE,related_name='pedidos_como_cliente')
-    repartidor = models.ForeignKey(User, on_delete=models.CASCADE,related_name='pedidos_como_repartidor')
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    fecha_recojo = models.DateTimeField()
-    fecha_entrega = models.DateTimeField()
+    repartidor = models.ForeignKey(User, on_delete=models.CASCADE,related_name='pedidos_como_repartidor',null=True, blank=True)
+    fecha_pedido = models.DateTimeField()
+    fecha_entrega = models.DateTimeField(null=True, blank=True)
     direccion = models.CharField(max_length=150)
-    estado = models.BooleanField(default=False)
+    estado = models.CharField(max_length=20, 
+        choices=[
+            ('pendiente', 'Pendiente'), 
+            ('en_proceso', 'En Proceso'), 
+            ('entregado', 'Entregado'),
+            ('cancelado', 'Cancelado')
+        ], default='pendiente')
     servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
     cantidad = models.IntegerField()
-    precio = models.DecimalField(decimal_places=2, max_digits=4)
+    precio_total = models.DecimalField(decimal_places=2, max_digits=4)
 
     def __str__(self):
-        return self.cliente.correo
+        return f"{self.cliente.correo} - {self.id}"
     class Meta:
         db_table = 'pedidos'
