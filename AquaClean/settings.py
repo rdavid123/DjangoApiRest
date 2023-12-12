078040bbc48339749c9eac7666e7168021397abf
David <david.ruiz@tecsup.edu.pe>
2023-12-12 02:37:18 -0500

    commit

--- AquaClean/settings.py
+++ AquaClean/settings.py
@@ -131,10 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/imagenes/'
-MEDIA_ROOT = BASE_DIR/ 'imagenes'
+MEDIA_ROOT = BASE_DIR/'imagenes'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- AquaClean/urls.py
+++ AquaClean/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('administrador.urls')),
 ]
-
-#urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
