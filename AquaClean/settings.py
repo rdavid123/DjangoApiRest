f8489c866eb70345d2b9ac19dab4a0fd624751c2
David <david.ruiz@tecsup.edu.pe>
2023-12-12 00:18:47 -0500

    commit

--- AquaClean/settings.py
+++ AquaClean/settings.py
@@ -146,6 +146,11 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR/'imagenes'
 MEDIA_URL = '/imagenes/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
